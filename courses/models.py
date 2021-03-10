e3c71827e14a7f9dfb26416b682cbf417b5e46c9
Kevin Stradtman <stradtkt22@gmail.com>
2021-03-10 05:22:57 -0500

    Added the beginning files

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,12 @@
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
+    teacher = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
--- learning/urls.py
+++ learning/urls.py
@@ -1,7 +1,15 @@
 
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^suggest/$', views.suggestion_view, name='suggestion'),
+    url(r'^$', views.hello_world, name='home'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
